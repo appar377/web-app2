7b698ba306303284fba4ae76ec40776fa1c24554
appar377 <riku.ren.sou@gmail.com>
2022-03-16 23:26:01 +0900

    コミット

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="conteiner">
+    <h1>SHARE</h1>
+    <NuxtLink to="/register">新規登録</NuxtLink>
+    <NuxtLink to="/login">新規登録</NuxtLink>
+    <div>
+      
+    </div>
+  </div>
+</template>
